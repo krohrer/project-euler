7fb4cf41ca082cbe30867e0231279c338bdc7a89
Kaspar M. Rohrer <kaspar.rohrer@gmail.com>
2013-03-21 19:08:08 +0100

    Extended Bitarray.

--- ocaml/bitarray.ml
+++ ocaml/bitarray.ml
@@ -15,3 +15,16 @@ let resize s n def =
   let s_new = make n def in
   String.blit s.data 0 s_new.data 0 (min n (length s));
   s.data <- s_new.data
+
+let fold_left ~f z s =
+  let n = length s in
+  let rec loop i acc =
+    if i < n then
+      let acc = f acc (get s i) in loop (i+1) acc
+    else
+      acc
+  in
+  loop 0 z
+
+let count_ones s =
+  fold_left ~f:(fun a b -> if b then a + 1 else a) 0 s
--- ocaml/bitarray.mli
+++ ocaml/bitarray.mli
@@ -7,3 +7,6 @@ val set : t -> int -> bool -> unit
 val get : t -> int -> bool
 
 val resize : t -> int -> bool -> unit
+
+val fold_left : f:('a -> bool -> 'a) -> 'a -> t -> 'a
+val count_ones : t -> int
