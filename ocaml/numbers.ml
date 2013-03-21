b370fd2c31dc7993e5d9258920645ab4fea6f4c6
Kaspar M. Rohrer <kaspar.rohrer@gmail.com>
2013-03-21 19:39:35 +0100

    Added Numbers.pow.

--- ocaml/numbers.ml
+++ ocaml/numbers.ml
@@ -8,3 +8,12 @@ let gcd a b =
 let gcd_fold1 = function
   | [] -> invalid_arg "Numbers.gcd_fold1"
   | x :: rest -> List.fold_left gcd x rest
+
+let rec pow n = function
+  | 0 -> 1
+  | 1 -> n
+  | e when e < 0 -> invalid_arg "Numbers.pow: negative exponent."
+  | e when e mod 2 == 0 -> (* even numbers *)
+    let p = pow n (e/2) in
+    p * p
+  | e -> n * pow n (e-1)
--- ocaml/numbers.mli
+++ ocaml/numbers.mli
@@ -1,3 +1,5 @@
 val gcd : int -> int -> int
 
 val gcd_fold1 : int list -> int
+
+val pow : int -> int -> int
